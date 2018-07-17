32e948e0422bbe0ac3026f094d93749cbda493ad
glathoud <glat@glat.info>
2018-07-17 18:56:15 +0200

    D: not so interesting at the moment: ldc2 -O too fast - at least on that one case

--- misc/d_performance_feasability/main.d
+++ misc/d_performance_feasability/main.d
@@ -1,52 +1,45 @@
 #!/usr/bin/env rdmd
+// -*- coding: utf-8 -*-
 
 import std.algorithm;
 import std.datetime.stopwatch : benchmark, StopWatch;
+import std.format;
 import std.math;
-import std.parallelism;
 import std.random;
 import std.stdio;
-
-enum  N = 200_000;
-enum BN = 100;
+import std.traits;
 
 /*
-dmd --version
-DMD64 D Compiler v2.078.3
+ldc2 -ofmain.bin -O main.d ; main.bin
 
-$ ./main.d 
-N:  200000
-BN: 100
+N:  100000000
+BN: 5
 
-1 sec, 80 ms, and 594 μs
-1 sec, 389 ms, 751 μs, and 4 hnsecs
-152 ms, 188 μs, and 1 hnsec
-126 ms, 478 μs, and 5 hnsecs
-283 ms, 427 μs, and 5 hnsecs
-192 ms, 43 μs, and 6 hnsecs
-342 ms, 349 μs, and 8 hnsecs
+686 ms, 336 μs, and 1 hnsec
+748 ms, 33 μs, and 8 hnsecs
+631 ms, 339 μs, and 5 hnsecs
+633 ms, 74 μs, and 7 hnsecs
+629 ms, 515 μs, and 8 hnsecs
 
----
+--
+dmd -ofmain.bin -O -inline main.d ; main.bin
 
-$ ldc2 --version
-LDC - the LLVM D compiler (1.10.0):
-  based on DMD v2.080.1 and LLVM 6.0.0
-  built with LDC - the LLVM D compiler (1.10.0)
-  Default target: x86_64-unknown-linux-gnu
+N:  100000000
+BN: 5
 
-$ ldc2  -ofmain.bin main.d ; main.bin
-N:  200000
-BN: 100
+N:  100000000
+BN: 5
 
-1 sec, 28 ms, and 536 μs
-1 sec, 408 ms, 192 μs, and 7 hnsecs
-162 ms, 944 μs, and 6 hnsecs
-158 ms, 897 μs, and 4 hnsecs
-326 ms, 268 μs, and 5 hnsecs
-236 ms, 584 μs, and 6 hnsecs
-352 ms, 594 μs, and 2 hnsecs
-
+4 secs, 542 ms, 993 μs, and 3 hnsecs
+6 secs, 210 ms, 470 μs, and 6 hnsecs
+984 ms, 86 μs, and 2 hnsecs
+987 ms, 23 μs, and 8 hnsecs
+982 ms, 187 μs, and 5 hnsecs
 */
+
+enum  N = 100_000_000;
+enum BN = 5;
+
 
 void main()
 {
@@ -55,19 +48,31 @@ void main()
   
 auto data = create_pseudo_random_arr();
 
+ auto true_result = map_filter_sum_impl( data ); 
+ 
 auto r = benchmark!
-  ( () => map_filter_sum_impl( data )
-      , () => map_cache_filter_sum_impl( data)
-      , () => direct_impl( data )
-      , () => direct_impl2( data )
-      , () => direct_impl2_parallel( data )
-      , () => direct_impl2_external!( a => a.p, isFinite, (a,b) => a+b)( data )
-      , () => direct_impl2_external_parallel!( a => a.p, isFinite, (a,b) => a+b)( data )
-      )( BN );
+  ( wrap!( true_result, map_filter_sum_impl, data )
+    , wrap!( true_result, map_cache_filter_sum_impl, data )
+    , wrap!( true_result, direct_impl, data )
+    , wrap!( true_result, direct_impl2, data )
+    , wrap!( true_result, direct_impl2_external!( a => a.p, isFinite, (a,b) => a+b), data )
+    )( BN );
 writeln;
 foreach (o; r)
 writeln( o );
 
+}
+
+// ---
+
+void wrap( alias true_result, alias impl, alias data )()
+{
+  auto obtained = impl( data );
+  auto delta = true_result - obtained;
+  bool isOk  = 1e-10 > abs( delta );
+  assert( isOk
+          , format( "true_result: %s, obtained: %s, delta: %s", true_result, obtained, delta )
+          );
 }
 
 // --- 
@@ -116,20 +121,6 @@ if (isFinite( current_1 ))
 return ret;
 }
 
-
-double direct_impl2_parallel( in ref S[] data )
-{
-double ret = 0;
-foreach (k, ref v; data.parallel ) // okay in this particular case
-  {
-alias current_0 = v;
-auto current_1 = current_0.p;
-if (isFinite( current_1 ))
-  ret += current_1;
-}
-return ret;
-}
-
 double direct_impl2_external( alias fa, alias fb, alias fc )( in ref S[] data )
 {
 double ret = 0;
@@ -142,21 +133,6 @@ if (fb( current_1 ))
 }
 return ret;
 }
-
-
-double direct_impl2_external_parallel( alias fa, alias fb, alias fc )( in ref S[] data )
-{
-double ret = 0;
-foreach (k, ref v; data.parallel ) // okay in this particular case
-  {
-alias current_0 = v;
-auto current_1 = fa( current_0 );
-if (fb( current_1 ))
-  ret = fc( ret, current_1 );
-}
-return ret;
-}
-
 
 // ---
 
